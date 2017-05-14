5304b1ba87af4f9801c3663db58ffdd828748808
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-05-14 19:46:54 +1000

    Done

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-module.exports = function () {
-    return 'hello1';
+module.exports = {
+    sayHello: function () {
+        return 'hello1';
+    },
+    addNumber: function (value1, value2) {
+        return value1+value2;
+    }
 };
--- test/appTest.js
+++ test/appTest.js
@@ -1,8 +1,25 @@
 const assert = require('chai').assert;
 const app = require('../app');
+//const sayHello = require('../app').sayHello;
+//const addNumbers = require('../app').addNumber;
 
 describe('App', function () {
-    it('app should return hello', function () {
-        assert.equal(app(), 'hello');
+    describe('sayHello', function () {
+        it('sayHello should return hello', function () {
+            var result = app.sayHello();
+            assert.equal(result, 'hello');
+        });
+
+        it('sayHello should return a type string', function () {
+            var result = app.sayHello();
+            assert.typeOf(result, 'string');
+        });
     });
+
+    describe('addNumbers', function () {
+        it('addNumbers should be above 5', function () {
+            var result = app.addNumbers(1,1);
+            assert.isAbove(result, 5);
+        });
+    })
 });
